b729c5c6cbde1b82f0638f37b47780757db15a00
karim551 <karimmuhammad551@gmail.com>
2023-05-24 02:37:29 +0700

    Complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Myap());
+  runApp(const Myapp());
 }
 
 class Myapp extends StatelessWidget {
@@ -21,7 +21,51 @@ class Myapp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text("Hello World")
+            Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Icon(Icons.account_circle, size: 50),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Flutter McFlutter",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      "Experienced App Developer",
+                      style: TextStyle(
+                        fontSize: 13,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 8.0, right: 8.0, bottom: 8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("123 Main Street"),
+                  Text("(415) 555-0198")
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Icon(Icons.accessibility_sharp),
+                Icon(Icons.timer_outlined),
+                Icon(Icons.phone_android_outlined),
+                Icon(Icons.phone_iphone),
+              ],
+            )
           ],
         ),
       ),
